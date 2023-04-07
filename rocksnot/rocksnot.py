8779de08764654a1319e334cbf125e30fa4b4fda
RobbyLape <robby.lape@gmail.com>
2023-04-07 08:49:41 -0500

    rocksnot.pyCommit

--- rocksnot/rocksnot.py
+++ rocksnot/rocksnot.py
@@ -1,1 +1,174 @@
 """Main module."""
+import string
+import random
+import ipyleaflet
+
+class Map(ipyleaflet.Map):
+    
+    def __init__(self, center=[20, 0], zoom=2, **kwargs) -> None:
+
+        if "scroll_wheel_zoom" not in kwargs:
+            kwargs["scroll_wheel_zoom"] = True
+
+        super().__init__(center=center, zoom=zoom, **kwargs)
+
+        if "height" not in kwargs:
+            self.layout.height = "500px"
+        else:
+            self.layout.height = kwargs["height"]
+
+        if "fullscreen_control" not in kwargs:
+            kwargs["fullscreen_control"] = True
+        if kwargs["fullscreen_control"]:
+            self.add_fullscreen_control()
+        
+        if "layers_control" not in kwargs:
+            kwargs["layers_control"] = True
+        if kwargs["layers_control"]:
+            self.add_layers_control()
+
+    def add_search_control(self, position="topleft", **kwargs):
+        """Adds a search control to the map.
+        Args:
+            kwargs: Keyword arguments to pass to the search control.
+        """
+        if "url" not in kwargs:
+            kwargs["url"] = 'https://nominatim.openstreetmap.org/search?format=json&q={s}'
+    
+
+        search_control = ipyleaflet.SearchControl(position=position, **kwargs)
+        self.add_control(search_control)
+
+    def add_draw_control(self, **kwargs):
+        """Adds a draw control to the map.
+        Args:
+            kwargs: Keyword arguments to pass to the draw control.
+        """
+        draw_control = ipyleaflet.DrawControl(**kwargs)
+
+        draw_control.polyline =  {
+            "shapeOptions": {
+                "color": "#6bc2e5",
+                "weight": 8,
+                "opacity": 1.0
+            }
+        }
+        draw_control.polygon = {
+            "shapeOptions": {
+                "fillColor": "#6be5c3",
+                "color": "#6be5c3",
+                "fillOpacity": 1.0
+            },
+            "drawError": {
+                "color": "#dd253b",
+                "message": "Oups!"
+            },
+            "allowIntersection": False
+        }
+        draw_control.circle = {
+            "shapeOptions": {
+                "fillColor": "#efed69",
+                "color": "#efed69",
+                "fillOpacity": 1.0
+            }
+        }
+        draw_control.rectangle = {
+            "shapeOptions": {
+                "fillColor": "#fca45d",
+                "color": "#fca45d",
+                "fillOpacity": 1.0
+            }
+        }
+
+        self.add_control(draw_control)
+
+    def add_layers_control(self, position="topright"):
+        """Adds a layers control to the map.
+        Args:
+            kwargs: Keyword arguments to pass to the layers control.
+        """
+        layers_control = ipyleaflet.LayersControl(position=position)
+        self.add_control(layers_control)
+
+    def add_fullscreen_control(self, position="topleft"):
+        """Adds a fullscreen control to the map.
+        Args:
+            kwargs: Keyword arguments to pass to the fullscreen control.
+        """
+        fullscreen_control = ipyleaflet.FullScreenControl(position=position)
+        self.add_control(fullscreen_control)
+
+    def add_tile_layer(self, url, name, attribution="", **kwargs):
+        """Adds a tile layer to the map.
+        Args:
+            url (str): The URL template of the tile layer.
+            attribution (str): The attribution of the tile layer.
+            name (str, optional): The name of the tile layer. Defaults to "OpenStreetMap".
+            kwargs: Keyword arguments to pass to the tile layer.
+        """
+        tile_layer = ipyleaflet.TileLayer(url=url, attribution=attribution, name=name, **kwargs)
+        self.add_layer(tile_layer)
+
+    def add_basemap(self, basemap):
+        """Adds a basemap to the map.
+        Args:
+            basemap (str): The name of the basemap to add.
+        """
+        import xyzservices.providers as xyz
+        try:
+            layer = eval(f"xyz.{basemap}")
+            url = layer.build_url()
+            attribution = layer.attribution
+            self.add_tile_layer(url=url, attribution=attribution, name=basemap)
+
+        except:
+            raise ValueError(f"Invalid basemap name: {basemap}")
+    
+
+    def add_geojson(self, data, **kwargs):
+        """Adds a GeoJSON layer to the map.
+        Args:
+            data (dict): The GeoJSON data.
+            kwargs: Keyword arguments to pass to the GeoJSON layer.
+        """
+        import json
+
+        if isinstance(data, str):
+            with open(data, "r") as f:
+                data = json.load(f)
+
+        geojson = ipyleaflet.GeoJSON(data=data, **kwargs)
+        self.add_layer(geojson)
+
+def generate_random_string(length=10, upper=False, digits=False, punctuation=False):
+    """Generates a random string of a given length.
+    Args:
+        length (int, optional): The length of the string to generate. Defaults to 10.
+        upper (bool, optional): Whether to include uppercase letters. Defaults to False.
+        digits (bool, optional): Whether to include digits. Defaults to False.
+        punctuation (bool, optional): Whether to include punctuation. Defaults to False.
+    Returns:
+        str: The generated string.
+    """
+
+    letters = string.ascii_lowercase
+    if upper:
+        letters += string.ascii_uppercase
+    if digits:
+        letters += string.digits
+    if punctuation:
+        letters += string.punctuation
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+def generate_lucky_number(length=1):
+    """Generates a random number of a given length.
+    Args:
+        length (int, optional): The length of the number to generate. Defaults to 1.
+    Returns:
+        int: The generated number.
+    """
+
+    result_str = ''.join(random.choice(string.digits) for i in range(length))
+    return int(result_str)
